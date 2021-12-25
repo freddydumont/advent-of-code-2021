fed45039b4c1ac2b44e56d32fb524c2501e712c1
Frederick Morin <frederickmorin18@gmail.com>
2021-12-25 00:24:49 -0500

    Solve d01 part 1

--- d01_sonar_sweep/src/main.rs
+++ d01_sonar_sweep/src/main.rs
@@ -1,3 +1,47 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_count_increases() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(7, count_increases(input));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open("src/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut input: Vec<u32> = vec![];
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    for line in reader.lines() {
+        let line = line.unwrap().parse::<u32>().unwrap();
+
+        input.push(line)
+    }
+
+    println!("{}", count_increases(input));
+}
+
+// count the number of increases in the sonar readings
+fn count_increases(input: Vec<u32>) -> u32 {
+    let mut count: u32 = 0;
+    let mut prev = input[0];
+
+    for depth in input {
+        if depth > prev {
+            count += 1;
+        }
+
+        prev = depth;
+    }
+
+    count
 }
