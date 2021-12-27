3f5d50186bae5a6a3a59fc85fab357d6ae79b533
Frederick Morin <frederickmorin18@gmail.com>
2021-12-26 21:35:16 -0500

    Implement part 2 and refactor to use refs

--- d01_sonar_sweep/src/main.rs
+++ d01_sonar_sweep/src/main.rs
@@ -9,7 +9,14 @@ mod tests {
     fn should_count_increases() {
         let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!(7, count_increases(input));
+        assert_eq!(7, count_increases(&input));
+    }
+
+    #[test]
+    fn should_count_increases_by_window() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(5, count_increases_by_window(&input));
     }
 }
 
@@ -18,7 +25,7 @@ fn main() {
     let file = File::open("src/input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut input: Vec<u32> = vec![];
+    let mut input: Vec<u32> = Vec::with_capacity(2000);
 
     // Read the file line by line using the lines() iterator from std::io::BufRead.
     for line in reader.lines() {
@@ -27,15 +34,41 @@ fn main() {
         input.push(line)
     }
 
-    println!("{}", count_increases(input));
+    println!(
+        "number of increases in sonar readings: {}",
+        count_increases(&input)
+    );
+    println!(
+        "number of increases in sonar reading windows: {}",
+        count_increases_by_window(&input)
+    );
 }
 
-// count the number of increases in the sonar readings
-fn count_increases(input: Vec<u32>) -> u32 {
+/// count the number of increases in the sonar readings
+fn count_increases(input: &Vec<u32>) -> u32 {
     let mut count: u32 = 0;
-    let mut prev = input[0];
+    let mut prev = &input[0];
 
     for depth in input {
+        if depth > prev {
+            count += 1;
+        }
+
+        prev = depth;
+    }
+
+    count
+}
+
+/// count the number of increases in the sonar readings by sliding window of 3 reading
+fn count_increases_by_window(input: &Vec<u32>) -> u32 {
+    let mut count: u32 = 0;
+    // prev is now the total of a window, starting with the first 3 elements
+    let mut prev = input[0] + input[1] + input[2];
+
+    for w in input.windows(3) {
+        let depth = w[0] + w[1] + w[2];
+
         if depth > prev {
             count += 1;
         }
