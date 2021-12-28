f02999df722aa1e76e105d302593910776f9592a
Frederick Morin <frederickmorin18@gmail.com>
2021-12-27 22:24:27 -0500

    Parse input into vec of Strings

--- d02_dive/src/main.rs
+++ d02_dive/src/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("src/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut input: Vec<String> = Vec::with_capacity(1000);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        input.push(line)
+    }
+
+    print!("{:#?}", input)
 }
