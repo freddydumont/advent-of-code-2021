f9809bf12deb8094911980f6b08e444617458c46
Frederick Morin <frederickmorin18@gmail.com>
2021-12-28 00:27:53 -0500

    Solve part 2

--- d02_dive/src/main.rs
+++ d02_dive/src/main.rs
@@ -16,6 +16,10 @@ fn main() {
     }
 
     println!("position x * y: {}", calculate_position(&input));
+    println!(
+        "position x * y (with aim): {}",
+        calculate_position_with_aim(&input)
+    );
 }
 
 /// returns the product of the final position's coordinates
@@ -42,6 +46,34 @@ fn calculate_position(input: &[String]) -> i32 {
     coordinates.x * coordinates.y
 }
 
+/// returns the product of the final position's coordinates and aim
+fn calculate_position_with_aim(input: &[String]) -> i32 {
+    struct Coordinates {
+        x: i32,
+        y: i32,
+        aim: i32,
+    }
+
+    let mut coordinates = Coordinates { x: 0, y: 0, aim: 0 };
+
+    for command in input {
+        let instruction: Vec<&str> = command.split(' ').collect();
+        let n = instruction[1].parse::<i32>().unwrap();
+
+        match instruction[0] {
+            "forward" => {
+                coordinates.x += n;
+                coordinates.y += coordinates.aim * n;
+            }
+            "down" => coordinates.aim += n,
+            "up" => coordinates.aim -= n,
+            _ => panic!("Error, invalid input."),
+        };
+    }
+
+    coordinates.x * coordinates.y
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -58,5 +90,19 @@ mod tests {
         ];
 
         assert_eq!(150, calculate_position(&input));
+    }
+
+    #[test]
+    fn should_calculate_position_with_aim() {
+        let input = vec![
+            "forward 5".to_string(),
+            "down 5".to_string(),
+            "forward 8".to_string(),
+            "up 3".to_string(),
+            "down 8".to_string(),
+            "forward 2".to_string(),
+        ];
+
+        assert_eq!(900, calculate_position_with_aim(&input));
     }
 }
