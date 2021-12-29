e63da32d10602db4eb6a55698be386c02b6b4633
Frederick Morin <frederickmorin18@gmail.com>
2021-12-29 00:14:39 -0500

    Solve d03 part 1

--- d03_binary_diagnostic/src/main.rs
+++ d03_binary_diagnostic/src/main.rs
@@ -15,10 +15,42 @@ fn main() {
         input.push(line)
     }
 
-    print!("{:#?}", input)
+    print!("power consumption: {}", calculate_power_consumption(&input))
 }
 
-fn calculate_power_consumption(input: &[String]) -> u32 {}
+/// takes a list of binary strings as input and returns the product of the gamma and epsilon values
+///
+/// **gamma**: most common bit in corresponding char of all strings
+/// **epsilon**: opposite of gamma
+fn calculate_power_consumption(input: &[String]) -> u32 {
+    let number_of_bits = input[0].chars().count();
+    let mut number_of_ones = vec![0; number_of_bits];
+    let mut gamma = vec!['0'; number_of_bits];
+    let mut epsilon = vec!['0'; number_of_bits];
+
+    for line in input {
+        for (i, binary) in line.chars().enumerate() {
+            if binary == '1' {
+                number_of_ones[i] += 1;
+            }
+        }
+    }
+
+    for (i, n) in number_of_ones.iter().enumerate() {
+        if n > &(input.len() / 2) {
+            gamma[i] = '1';
+        } else {
+            epsilon[i] = '1';
+        }
+    }
+
+    let gamma_decimal = u32::from_str_radix(&gamma.iter().collect::<String>(), 2).unwrap();
+    let epsilon_decimal = u32::from_str_radix(&epsilon.iter().collect::<String>(), 2).unwrap();
+
+    print!("gamma: {}\nepsilon: {}\n", gamma_decimal, epsilon_decimal);
+
+    gamma_decimal * epsilon_decimal
+}
 
 #[cfg(test)]
 mod tests {
